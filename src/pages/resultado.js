5508eec6c0a418ea8621c0443fe57512943ab05f
Nicolás Meléndez <nicomelendez10@gmail.com>
2023-02-22 18:17:57 -0300

    two-up de  nuevo

--- src/pages/resultado.js
+++ src/pages/resultado.js
@@ -1,4 +1,6 @@
+import useEditor from '@/hooks/useEditor';
 import Layout from '@/layout/Layout'
+import "two-up-element"
 import React, { useEffect, useState } from 'react'
 
 export default function Resultado() {
@@ -14,10 +16,10 @@ export default function Resultado() {
       <div className='w-full flex flex-col pt-5 space-y-5 items-center justify-center mx-auto'>
         <h2 className='text-xl text-white'>¡Resultado final!</h2>
         {showTwoUp && (
-          <div className="two-up">
+           <two-up>
             <img className="lg:max-h-[500px]" src={imageOriginal} alt="Imagen original subida por el usuario" />
             <img className="lg:max-h-[500px]" src={imageModificada} alt="Imagen sin fondo subida por el usuario" />
-          </div>
+          </two-up>
         )}
         <div className='text-center pt-5'>
          <a download href={imageModificada} className="hover:cursor-pointer hover:underline text-white rounded-full bg-gradient-to-r from-blue-500 to-violet-600 mt-10 text-bold px-6 py-4 text-center mx-auto">Descargar imagen</a>
