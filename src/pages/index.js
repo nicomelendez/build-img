300e4820e410eea67208f052e55486e7711b2e43
Nicolás Meléndez <nicomelendez10@gmail.com>
2023-02-23 19:09:05 -0300

    ahora si

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Layout pagina='Inicio' >
       <div className='h-full flex flex-col items-center justify-center'>
-      <h2 className='text-center text-4xl font-black text-white uppercase pb-5 sombra' style={{ lineHeight: '1.2', wordWrap: 'break-word' }}>
+      <h2 className='text-center px-2 text-2xl lg:text-4xl font-black text-white uppercase pb-5 sombra' style={{ lineHeight: '1.2', wordWrap: 'break-word' }}>
         <span className='block'>¡Sube una <span className='bg-gradient-to-r text-transparent from-blue-500 to-violet-600 bg-clip-text'>imagen</span></span> y comienza a <span className='bg-gradient-to-r text-transparent from-blue-500 to-violet-600 bg-clip-text'>personalizarla</span>!
       </h2>
         <FormImage />
