ddc80044fcf2d5f2152a8366ec9e6c04596c7538
Nicolás Meléndez <nicomelendez10@gmail.com>
2023-02-28 18:46:18 -0300

    Componentes que no uso

--- src/components/Comparacion.jsx
+++
@@ -1,5 +0,0 @@
-export default function Comparacion() {
-  return (
-    <div>Comparacion</div>
-  )
-}
--- src/components/NavHamurgesa.jsx
+++
@@ -1,5 +0,0 @@
-export default function NavHamurgesa() {
-  return (
-    <div>NavHamurgesa</div>
-  )
-}
