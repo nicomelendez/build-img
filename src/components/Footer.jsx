081679d99bc00cd991f29fd0a880a75072b681e7
Nicolás Meléndez <nicomelendez10@gmail.com>
2023-03-05 19:01:02 -0300

    sacar un icon del contacto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,22 +38,6 @@ export default function Footer() {
               ></path>
             </svg>
           </a>
-          <a className="social-link" href="#">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="800"
-              height="800"
-              fill="#000"
-              version="1.1"
-              viewBox="0 0 330.001 330.001"
-              xmlSpace="preserve"
-            >
-              <g>
-                <path d="M173.871 177.097a14.982 14.982 0 01-8.87 2.903 14.98 14.98 0 01-8.871-2.903L30 84.602.001 62.603 0 275.001c.001 8.284 6.716 15 15 15L315.001 290c8.285 0 15-6.716 15-14.999V62.602l-30.001 22-126.129 92.495z"></path>
-                <path d="M165.001 146.4L310.087 40.001 19.911 40z"></path>
-              </g>
-            </svg>
-          </a>
           <a
             className="social-link"
             href="https://www.linkedin.com/in/nicol%C3%A1s-mel%C3%A9ndez-60ba32160/"
